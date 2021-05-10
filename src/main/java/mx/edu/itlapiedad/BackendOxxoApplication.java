304a9b6a72911d6f5dc86b28e5760dd35427ea9e
luis <luisenrique14640047@gmail.com>
2021-05-09 20:12:04 -0500

    commit inicial

---
+++ src/main/java/mx/edu/itlapiedad/BackendOxxoApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.itlapiedad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOxxoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendOxxoApplication.class, args);
+	}
+
+}
